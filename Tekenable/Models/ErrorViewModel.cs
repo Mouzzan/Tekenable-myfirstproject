5b9735ba00d0880f1d7ca9e66d6200f862812120
MouzzanMunir <MouzzanMunir@TEKPKLPT2212>
2022-04-01 14:23:03 +0500

    Add project files.

---
+++ Tekenable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tekenable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
